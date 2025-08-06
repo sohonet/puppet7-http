46d621da332f4e95823045d95e892dd1ccdc233d
xraystyle <ben.levin@sohonet.com>
2025-08-06 16:11:10 -0700

    Add ruby dev package so that puma will install correctly.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,13 @@
 class http {
 
+  package { 'ruby-dev':
+    ensure => installed,
+  }
+
   package { ['sinatra','rack','webrick', 'rackup', 'puma']:
     ensure   => present,
     provider => 'gem',
+    require  => Package['ruby-dev'],
   }
 
 }
